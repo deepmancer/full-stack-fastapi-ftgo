32550734a90e56829b4c14ad89cfbdc38217bc49
hamidrezayaghobi <Hamidrezayaghobi021@gmail.com>
2024-08-03 13:03:52 +0330

    Fix Error Handling

--- backend/gateway/src/middleware/timing/handler.py
+++ backend/gateway/src/middleware/timing/handler.py
@@ -7,5 +7,6 @@ class ProcessTimeMiddleware(BaseHTTPMiddleware):
         start_time = time.time()
         response = await call_next(request)
         process_time = time.time() - start_time
-        response.headers["X-Process-Time"] = str(process_time)
+        if response is not None:
+            response.headers["X-Process-Time"] = str(process_time)
         return response
